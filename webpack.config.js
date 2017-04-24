1174ebadd58f8ff45d5cdc21e512425cf0e56f99
Nick Rigby <nick.rigby@gmail.com>
2017-04-24 11:39:54 -0400

    Production build now excludes dev server code

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const extractSass = new ExtractTextPlugin({
     filename: "index.css"
 });
 
-module.exports = {
+const config = {
     entry: "./src/js/index.ts",
     output: {
         filename: "bundle.js",
@@ -28,15 +28,32 @@ module.exports = {
                     }],
                     fallback: 'style-loader'
                 })
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 1000
+                }
             }
         ]
     },
     plugins: [
-        extractSass,
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
+        extractSass
+    ]
+};
+
+if(process.env.NODE_ENV !== 'production') {
+
+    config.devServer = {
         hot: true,
         publicPath: '/build/'
-    }
-};
+    };
+
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin()
+    );
+
+}
+
+module.exports = config;
